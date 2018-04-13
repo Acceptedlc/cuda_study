425e7b1af741eda552abcdfa18a8a5a21abe1a4d
lichao <lichao@gagougroup.com>
2018-04-13 17:58:24 +0800

    feat: test cublus

---
+++ cublus_test.cu
@@ -0,0 +1,14 @@
+/* Includes, system */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Includes, cuda */
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#include <helper_cuda.h>
+
+int main(int argc, char **argv) {
+	cublasHandle_t handle;
+	status = cublasCreate(&handle);
+}
