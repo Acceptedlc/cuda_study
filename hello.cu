4a52ecf60c79fb1bf6da5e562a7f631586d7a78c
lichao <lichao@gagougroup.com>
2018-04-13 10:56:20 +0800

    fix: hello

--- hello.cu
+++ hello.cu
@@ -1,16 +1,38 @@
-__global__ void helloFromGPU (void)
+#include <stdio.h>
+ 
+const int N = 16; 
+const int blocksize = 16; 
+ 
+__global__ 
+void hello(char *a, int *b) 
 {
-    printf(“Hello World from GPU!\n”);
+	a[threadIdx.x] += b[threadIdx.x];
 }
-
-int main(void)
+ 
+int main()
 {
-// hello from cpu
-printf(“Hello World from CPU!\n”);
-
-helloFromGPU <<<1, 10>>>();
-
-cudaDeviceReset();
-
-return 0;
+	char a[N] = "Hello \0\0\0\0\0\0";
+	int b[N] = {15, 10, 6, 0, -11, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+ 
+	char *ad;
+	int *bd;
+	const int csize = N*sizeof(char);
+	const int isize = N*sizeof(int);
+ 
+	printf("%s", a);
+ 
+	cudaMalloc( (void**)&ad, csize ); 
+	cudaMalloc( (void**)&bd, isize ); 
+	cudaMemcpy( ad, a, csize, cudaMemcpyHostToDevice ); 
+	cudaMemcpy( bd, b, isize, cudaMemcpyHostToDevice ); 
+	
+	dim3 dimBlock( blocksize, 1 );
+	dim3 dimGrid( 1, 1 );
+	hello<<<dimGrid, dimBlock>>>(ad, bd);
+	cudaMemcpy( a, ad, csize, cudaMemcpyDeviceToHost ); 
+	cudaFree( ad );
+	cudaFree( bd );
+	
+	printf("%s\n", a);
+	return EXIT_SUCCESS;
 }
